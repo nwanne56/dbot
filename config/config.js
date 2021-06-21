73c1b12126305d50510ea8a59415cee60ba24de9
David O <bficmdbroadcast@gmail.com>
2021-06-21 19:33:00 -0400

    additions

---
+++ config/config.js
@@ -0,0 +1,12 @@
+let config = {}
+config.prefix = "dy!";
+config.basic = {
+    "server": {
+        "prefix": "dy!",
+        "customenabled": false,
+        "custom": [{ 
+            "test" : { "text": "this is a test" }
+        }]
+    }
+}
+export {config}
